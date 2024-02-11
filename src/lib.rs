84e47d6ee12d137ea7d3aa026e2753124aabdc81
Ethan Nguyen <manhnguyend8@gmail.com>
2024-02-11 22:51:39 +0000

    Change to iterators

--- src/lib.rs
+++ src/lib.rs
@@ -39,13 +39,10 @@ pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a st
 /// the given query string. Returns a vector of references
 /// to the matched lines. Performs a case-sensitive search.
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-    results
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub struct Config {
@@ -63,12 +60,22 @@ pub struct Config {
 /// Returns a Result with the Config or a static error string if there are
 /// not enough arguments.
 impl Config {
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
+    pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        // The first argument should be the name of the package
+        args.next();
+
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => {
+                return Err("Didn't get a query string");
+            }
+        };
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => {
+                return Err("Didn't get a file path");
+            }
+        };
         let ignore_case = env::var("IGNORE_CASE").is_ok();
 
         Ok(Config {
